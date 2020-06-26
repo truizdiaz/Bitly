950712a3471d1aae0ace2f6248eada3d003828ca
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-26 10:33:57 -0300

    ShortUrlService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShortUrlService {
+  urlBitly = 'https://api-ssl.bitly.com/v4/bitlinks';
+  token = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUrlShort(url: string): Observable<any> {
+    const token = this.getToken();
+    const body = {
+      long_url: url
+    };
+
+    return this.http.post(this.urlBitly, body, { headers: token });
+  }
+
+  getToken() {
+    const tokenHeader = new HttpHeaders({Authorization: 'Bearer' + this.token});
+    return tokenHeader;
+  }
 }
