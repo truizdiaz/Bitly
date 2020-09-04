2e4ca664b9d52822d305c9fdd94f55722956a916
truizdiaz <tomiruizdiaz@gmail.com>
2020-09-04 15:11:12 -0300

    Terminado

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShortUrlService } from '../services/short-url.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +10,17 @@ export class DashboardComponent implements OnInit {
   btnString = 'Shorter';
   url = '';
   urlShort = '';
-  constructor() { }
+  constructor(private shortUrlService: ShortUrlService) { }
 
   ngOnInit(): void {
   }
 
   shorter() {
     if (this.btnString === 'Shorter') {
+      this.shortUrlService.getUrlShort(this.url).subscribe(data => {
+        console.log(data);
+        this.urlShort = data.link;
+      });
       this.btnString = 'Reset';
     } else {
       this.btnString = 'Shorter';
--- src/app/services/short.service.ts
+++ src/app/services/short.service.ts
@@ -2,26 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class ShortUrlService {
+export class ShortService {
   urlBitly = 'https://api-ssl.bitly.com/v4/bitlinks';
-  token = 'e06ee00fac1f1f108cee64b3967a703c8f4aa27a';
+  token = '81c5bc644ff2c3c008f4ea79456cc9d34e7cc806';
+  
 
   constructor(private http: HttpClient) { }
 
-  getUrlShort(url: string): Observable<any> {
+  getLinkShort(url): Observable<any> {
     const token = this.getToken();
-    const body = {
+    const body  = {
       long_url: url
     };
-
-    return this.http.post(this.urlBitly, body, { headers: token });
+    return this.http.post(this.urlBitly, body, { headers: token});
   }
 
   getToken() {
-    const tokenHeader = new HttpHeaders({Authorization: 'Bearer ' + this.token});
-    return tokenHeader;
+    const tokenHeader1 = new HttpHeaders({Authorization: 'Bearer ' + this.token});
+    return tokenHeader1;
   }
 }
